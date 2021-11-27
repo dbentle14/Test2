99fee9e3521a1975710006f7f94970484abe2e3e
dbentle14 <89591011+dbentle14@users.noreply.github.com>
2021-11-27 09:06:27 -0600

    Create Data-Types.js

---
+++ Data-Types.js
@@ -0,0 +1,23 @@
+var number = 10;
+
+console.log(number);
+
+
+// string
+var str = "This is a string"
+console.log(str);
+console.log(typeof str);
+
+// array
+var cars = ["Honda", "Volvo", "BMW"];
+console.log(cars);
+console.log(typeof cars);
+
+// Object
+var obj = {name: 'Jon', City: 'NY'};
+console.log(obj);
+console.log(typeof obj);
+
+var u;
+console.log(u);
+console.log(typeof u);
