27d9c072764a94f3af40909b3ab1f6119c9cf536
dbentle14 <dbentle14@yahoo.com>
2021-11-28 08:15:17 -0600

    4th Commit

---
+++ Functions.js
@@ -0,0 +1,11 @@
+
+// General Function Syntax
+// function <nameOfFunction> (<parameateres>) { }
+
+function sum(num1, num2){
+    console.log(num1+num2)
+    return num1+num2;
+}
+
+const result = sum (4, 9);
+console.log(result);
