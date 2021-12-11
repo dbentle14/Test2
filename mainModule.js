e39a9f857d810257b17284a3b0e5a666d459a111
dbentle14 <dbentle14@yahoo.com>
2021-12-11 11:06:32 -0600

    Adding Resturant Project 01

---
+++ mainModule.js
@@ -0,0 +1,7 @@
+
+const mathModule = require("./mathModule");
+
+console.log("Add: "+mathModule.add(1, 2));
+console.log("Sub: "+mathModule.sub(4, 2));
+console.log("Mul: "+mathModule.mul(3, 2));
+console.log("Div: "+mathModule.div(6, 2));
